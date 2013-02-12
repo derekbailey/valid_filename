d6a4ddaf3dfd4a5a15d6b1dad6ce913bfbd12e8e
derekbailey <4uauaua@gmail.com>
2013-02-12 09:21:39 +0900

    fix typo

--- lib/valid_filename.rb
+++ lib/valid_filename.rb
@@ -2,15 +2,15 @@ require "valid_filename/version"
 
 module ValidFilename
   INVALID_CHARS = '\\\/:*?"<>|'
-  REPACE_STRING = "_"
+  REPLACE_STRING = "_"
 
-  def self.replace(str, c = REPACE_STRING)
+  def self.replace(str, c = REPLACE_STRING)
     str.gsub Regexp.new("[" + INVALID_CHARS + "]"), c
   end
 end
 
 class String
-  def valid_filename(rep = ValidFilename::REPACE_STRING)
+  def valid_filename(rep = ValidFilename::REPLACE_STRING)
     ValidFilename.replace(self, rep)
   end
 end
